08e0ce767ab1b4a86355b44d0fd20e418cfaa6f8
アーバンゆうた <zanka.bgl@gmail.com>
2026-04-01 20:16:18 +0900

    snap

--- src/adjustment_helper.py
+++ src/adjustment_helper.py
@@ -57,9 +57,9 @@ def main():
                 sys.stdout.write(UP_CURSOR * num_display_lines)
 
             lines = [
-                f"{CLEAR_LINE}frequency  : {target_f/1e6:10.6f} MHz -> {get_color_text(target_f, CENTER_FREQ, 'Hz')}",
-                f"{CLEAR_LINE}reactance  : {target_z.imag:10.6f} Ω     -> {get_color_text(target_z.imag, 0, 'Ω')}",
-                f"{CLEAR_LINE}resistance : {target_z.real:10.6f} Ω     -> {get_color_text(target_z.real, 50.0, 'Ω')}"
+                f"{CLEAR_LINE}frequency  : {target_f/1e6:10.6f } MHz -> {get_color_text(target_f, CENTER_FREQ, 'Hz')}",
+                f"{CLEAR_LINE}reactance  : {target_z.imag:10.6f} Ω   -> {get_color_text(target_z.imag, 0, 'Ω')}",
+                f"{CLEAR_LINE}resistance : {target_z.real:10.6f} Ω   -> {get_color_text(target_z.real, 50.0, 'Ω')}"
             ]
 
             sys.stdout.write("\n".join(lines) + "\n")
